dad94831ef591fde6550a31a64ba442225c58fc8
Iva Mladzhova <iva_mladjova0031@abv.bg>
2022-03-10 07:26:29 +0200

    nine.homework

--- src/main/java/HomeSixPasswod/Password.java
+++ src/main/java/HomeSixPasswod/Password.java
@@ -50,38 +50,9 @@ public class Password {
     }
 
     //Enter your password
-    public static void checkPassword(String pwd) {
-        int letterCount = 0;
-        int digitCount = 0;
-        int symbolCount = 0;
-        if (pwd.length() >= 7) {
-            for (int i = 0; i < pwd.length(); i++) {
-                if (Character.isLetter(pwd.charAt(i))) {
-                    letterCount++;
-                }
-                if (Character.isDigit(pwd.charAt(i))) {
-                    digitCount++;
-                }
-                if ((pwd.contains("!")) || (pwd.contains("&")) || (pwd.contains("@"))) {
-                    symbolCount++;
-                }
-            }
-            if ((letterCount == 0) && (digitCount > 0) && (symbolCount > 0)) {
-                System.out.println("Valid password!");
-            } else {
-                if (letterCount == 0) {
-                    System.out.println("Password must contain letter!");
-                }
-                if (symbolCount == 0) {
-                    System.out.println("Password must contain digit!");
-                }
-                if (symbolCount == 0) {
-                    System.out.println("Password must contain symbol ! & @ * / .");
-                }
-            }
-        } else {
-            System.out.println("Password must be minimum 8 symbols!");
 
-        }
+
     }
-}
+
+
+
